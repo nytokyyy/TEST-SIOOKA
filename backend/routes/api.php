56e893d76b0c14dcfa988e14f6c943b59d4df745
nytokyyy <tokyronaldo@gmail.com>
2026-02-14 22:55:37 +0300

    [FEATURE] Auth API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\Auth\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+/** AUTH START */
+Route::controller(UserAuthController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', 'logout');
+    });
+});
+/** AUTH END */
