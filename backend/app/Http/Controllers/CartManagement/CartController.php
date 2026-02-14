4ab6f302babe293100f9e6512938cedae6da9bf2
nytokyyy <tokyronaldo@gmail.com>
2026-02-14 23:40:11 +0300

    [FEATURE] cart management models + mirations + controllers

---
+++ backend/app/Http/Controllers/CartManagement/CartController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\CartManagement;
+
+use App\Http\Controllers\Controller;
+use App\Models\CartManagement\Cart;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cart $cart)
+    {
+        //
+    }
+}
