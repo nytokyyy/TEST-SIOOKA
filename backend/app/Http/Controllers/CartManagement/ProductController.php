15068af26d83b7035c99b8dae39cac38b4848b14
nytokyyy <tokyronaldo@gmail.com>
2026-02-15 21:50:04 +0300

    config product controller

--- backend/app/Http/Controllers/CartManagement/ProductController.php
+++ backend/app/Http/Controllers/CartManagement/ProductController.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CartManagement\ProductResource;
 use App\Models\CartManagement\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of products.
      */
-    public function index(Request $request)
+    public function index(Request $request): ResourceCollection
     {
         $perPage = $request->integer('per_page', 10);
 
@@ -30,7 +31,7 @@ class ProductController extends Controller
     /**
      * Display the specified product.
      */
-    public function show(Product $product)
+    public function show(Product $product): ProductResource
     {
         return new ProductResource($product);
     }
