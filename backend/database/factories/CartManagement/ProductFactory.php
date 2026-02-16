d2180161f7584e4f839008bb629c068f97001716
nytokyyy <tokyronaldo@gmail.com>
2026-02-16 14:51:16 +0300

    product factory name

--- backend/database/factories/CartManagement/ProductFactory.php
+++ backend/database/factories/CartManagement/ProductFactory.php
@@ -20,7 +20,7 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word(),
+            'name' => $this->faker->catchPhrase(),
             'price' => $this->faker->randomFloat(2, 1, 100)
         ];
     }
